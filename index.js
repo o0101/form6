3d8aefe2e3b5a16ef17f04a390d9622e0418bc32
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2019-03-27 22:36:53 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 function enhanceForm(formEl) {
   const walker = document.createTreeWalker(formEl, NodeFilter.SHOW_ELEMENT);
+  const fieldStack = [];
   while(walker.nextNode()) {
     const node = walker.currentNode;
+    if ( ! node.hasAttribute('name') ) continue;
+    // we need to do this because the walker does NOT show us closing tags
+    // only NEXT tag in in-order DFS
+    let parentIndex = fieldStack.length;
+    while(parentIndex--) {
+      const deepestParent = fieldStack[parentIndex];
+      if ( deepestParent.contains(node) ) break;
+    }
+    fieldStack.length = parentIndex+1;
     switch(node.localName) {
       case "fieldset": {
         // push name to stack
+        fieldStack.push(node);
       }
       default: {
-        if ( node.hasAttribute('name') ) {
-          // convert name to nested
-        }
+        // convert name to nested
+        const fieldNames = fieldStack.map(node => node.getAttribute('name'));
+        const fieldPath = `${fieldNames.shift()}${
+          fieldNames.map(name => `[${name}]`)
+        }[${node.getAttribute('name')}]`;
+        node.setAttribute('name', fieldPath);      
       }
     }
   }
